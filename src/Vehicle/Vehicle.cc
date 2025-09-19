b9a1ea9053ea08ddbbed7fc23739b8412efb05d6
Duc Thang <88826829+ThangDuc3101@users.noreply.github.com>
2025-09-19 07:10:28 +0700

    complete receive info from uav's server.

--- src/Vehicle/Vehicle.cc
+++ src/Vehicle/Vehicle.cc
@@ -201,7 +201,10 @@ Vehicle::Vehicle(LinkInterface*             link,
     connect(_requestTimer, &QTimer::timeout, this, &Vehicle::_sendRequest);
     connect(_networkManager, &QNetworkAccessManager::finished, this, &Vehicle::_requestFinished);
 
-    GetUAVInfo("http://127.0.0.1:5000/uav_info", 1);
+    // GetUAVInfo("http://127.0.0.1:5000/uav_info", 1);
+
+    GetUAVInfo("http://192.168.144.30:5000/uav_info", 1);
+
     //
 }
 
